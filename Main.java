f2e10095f2c542ef8140944a27b98b8d07498d2a
mileyvo <mtvo106@student.hbuhsd.edu>
2025-10-27 18:21:42 +0000

    10/27/2025

--- Main.java
+++ Main.java
@@ -9,12 +9,27 @@ public class Main
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      String current_letter = word.substring(i, i+1);
+      for (int j = 0; j < N; j++)
+      {
+        System.out.print(current_letter + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    for (int num = 10; num > 0; num--)
+    {
+      for (int newNum = 0; newNum < num; newNum++)
+      {
+        System.out.print(newNum + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
