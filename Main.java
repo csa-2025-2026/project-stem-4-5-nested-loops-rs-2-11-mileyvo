86b24b607f2659aae34245fb7521c1e323d6a4c6
mileyvo <mtvo106@student.hbuhsd.edu>
2025-10-28 18:21:25 +0000

    10/28/2025

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    printNTimes("code", 3);
+    printNums();
+    uprightNumberTriangle(10);
   }
 
   public static void printNTimes(String word, int N)
@@ -14,17 +16,16 @@ public class Main
       String current_letter = word.substring(i, i+1);
       for (int j = 0; j < N; j++)
       {
-        System.out.print(current_letter + " ");
+        System.out.print(current_letter + "");
       }
-      System.out.println();
     }
   }
 
   public static void printNums()
   {
-    for (int num = 10; num > 0; num--)
+    for (int num = 10; num >= 1; num--)
     {
-      for (int newNum = 0; newNum < num; newNum++)
+      for (int newNum = 1; newNum < num; newNum++)
       {
         System.out.print(newNum + " ");
       }
@@ -34,7 +35,15 @@ public class Main
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for (int num = 1; num <= N; num++)
+    {
+      // System.out.println("HI");
+      for (int newNum = 1; newNum <= num; newNum++)
+      {
+        System.out.print(newNum + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void starTree()
